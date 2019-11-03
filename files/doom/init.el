a9fe1b0a3f746a9690fcd6014b578767ea5c3722
Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>
2019-11-03 12:27:23 +0100

    doom: enable Rust layer
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>

--- files/doom/init.el
+++ files/doom/init.el
@@ -146,7 +146,7 @@
        ;; racket ; a DSL for DSLs
        rest ; Emacs as a REST client
        ;; ruby   ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp) ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        sh     ; she sells (ba|z|fi)sh shells on the C xor
        ;;solidity          ; do you need a blockchain? No.
