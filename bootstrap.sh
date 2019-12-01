a190c346eb73c8e78970be6509bb8d45818e8d6e
Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>
2019-12-01 23:35:58 +0100

    bootstrap: update command
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
-nix-shell -p _1password --command "./install.sh"
+nix-shell -p '_1password ' --command "./install.sh"
